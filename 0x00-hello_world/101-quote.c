df95943b5db11628e89b2e5b796312c2d6adf5d4
Mohammed <mohammedthe11th@gmail.com>
2022-10-13 22:31:26 +0300

    using the perror

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a message
+ *
+ * Return: returns a value of 1
+*/
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
+
