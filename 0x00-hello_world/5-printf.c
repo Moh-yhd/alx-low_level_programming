aa5189325311443ce41deb07191f6023e71816c9
Mohammed <mohammedthe11th@gmail.com>
2022-10-13 18:07:10 +0300

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sting "hello world"
+ *
+ * Return: returns the value zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
