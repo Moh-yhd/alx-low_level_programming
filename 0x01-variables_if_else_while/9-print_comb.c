f59a29bd75e209b7e9031df9120003041cee2d9e
Mohammed <mohammedthe11th@gmail.com>
2022-10-14 23:19:12 +0300

    putchar is starnge

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main- possible combination of single digit numbers"
+*
+* Return: zero is success
+*/
+
+int main(void)
+{
+	int a = '0';
+	char co = ',';
+	char sp = ' ';
+
+	while (a >= '0' && a <= '9')
+	{
+		putchar(a);
+		putchar(co);
+		if (a <= 9)
+		{
+			putchar(co);
+			putchar(sp);
+		}
+		a = a + 1;
+	}
+	putchar('\n');
+	return (0);
+}
