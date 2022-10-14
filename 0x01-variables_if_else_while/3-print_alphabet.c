2a390540f4512f65123d7e02b28b9760d165a9db
Mohammed_Android <mohammedthe11th@gmail.com>
2022-10-14 19:22:10 +0300

    lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main- checks if a random number is a positive, a negative or zero"
+*
+* Return: zero is success
+*/
+
+int main(void)
+{
+	char start = 'a';
+	char end = 'z';
+	char ch = start;
+	char start1 = 'A';
+	char end1 = 'Z';
+	char ch1 = start1;
+	while (ch <= end)
+	{
+		putchar(ch);
+		ch = ch + 1;
+	}
+	while (ch1 <= end1)
+	{
+		putchar(ch1);
+		ch1 = ch1 + 1;
+	}
+	putchar('\n');
+	return (0);
+}
