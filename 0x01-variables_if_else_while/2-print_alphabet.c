bdd054e74ebde68236a3b4c6bba2d3148ab8535f
Mohammed <mohammedthe11th@gmail.com>
2022-10-14 11:30:34 +0300

    checking putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main- checks if a random number is a positive, a negative or zero"
+*
+* Return: zero is success
+*/
+
+int main(void)
+{
+	putchar("abcdefghijklmnopqrstuvwxyz\n");
+	return (0);
+}
