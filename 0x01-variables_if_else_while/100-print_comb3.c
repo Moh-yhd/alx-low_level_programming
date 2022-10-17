90d4a846b9a4ffd3c329a10da8a94b1ceaff55de
Mohammed <mohammedthe11th@gmail.com>
2022-10-17 09:47:46 +0300

    finally did it using nested while loops and putchar

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int i = '0';
-    int j;
+/**
+ * main- prints all possbile combinations of single digit numbers"
+ *
+ * Return: zero is success
+*/
 
-    while (i <= '0')
-    {
-        j = '0';
-        while (j <= '9')
-        {
-           
-	if (i != j + '1')
-            putchar(i);
-            putchar(j);
-           
-            if (i < '9' && j < '9' && i != j + 1 && i != j + 1) //this works when the variables are int type using printf
-            {
-            putchar(44);
-            putchar(32);
-            }
-            j = j + 1;
-        }
-        i = i + 1;
-    }
-    putchar("\n");
-    return 0;
+int main(void)
+{
+	int i = 48;
+	int j;
+	int co = ',';
+	int sp = ' ';
+
+	while (i >= 48 && i <= 57)
+	{
+		j = 48;
+		while (j >= 48 && j <= 57)
+		{
+			if (i != j + 1)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i + j < 114 && i != j + 1)
+			{
+				putchar(co);
+				putchar(sp);
+			}
+			j = j + 1;
+		}
+		i = i + 1;
+	}
+	putchar('\n');
 }
+
