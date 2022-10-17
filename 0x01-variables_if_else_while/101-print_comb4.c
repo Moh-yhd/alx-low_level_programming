ca3cedb0a0b75dfdeb3ee42919ddce8e9ef9b789
Mohammed <mohammedthe11th@gmail.com>
2022-10-17 11:16:58 +0300

    uisng three nested  while loops

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- prints all possbile combinations of single digit numbers"
+ *
+ * Return: zero is success
+*/
+
+int main(void)
+{
+	int i = 48;
+	int j;
+	int k;
+	int co = ',';
+	int sp = ' ';
+
+	while (i >= 48 && i <= 57)
+	{
+		j = 48;
+		while (j >= 48 && j <= 57)
+		{	
+			k = 48;
+			while (k >= 48 && k <= 57)
+			{
+			if (i < j && j < k)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+			}
+			if (i + j < 113 && i < j && j < k)
+			{
+				putchar(co);
+				putchar(sp);
+			}
+			k = k + 1;
+			}
+			j = j + 1;
+		}
+		i = i + 1;
+	}
+	putchar('\n');
+	return (0);
+}
