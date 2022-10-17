e3d183e789c368f6ccf6debb39090ff1f23cd41c
Mohammed_Android <mohammedthe11th@gmail.com>
2022-10-17 21:52:24 +0300

    pushing

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,12 +9,32 @@
 int main(void)
 {
 	int i = 48;
+	int j;
+	int k;
+	int l;
 
 	while (i >= 48 && i <= 57)
 	{
-		putchar(i);
+		j = 48;
+		while (j >= 48 && j <= 57)
+		{
+			k = 48;
+			while (k >= 48 && k >= 57)
+			{
+				l = 48;
+					while (l >= 48 && j <= 57)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(k);
+						putchar(l);
+						l = l + 1;
+					}
+					k = k + 1;
+			}
+			j = j + 1;
+		}
 		i = i + 1;
-
 	}
-	return(0);
+	return (0);
 }
