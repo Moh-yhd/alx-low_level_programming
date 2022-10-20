55935317abc55eff39f9adbb026b715a0ca7b8c8
Mohammed_Android <mohammedthe11th@gmail.com>
2022-10-20 12:22:42 +0300

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: is an integer
+ * @b: is an integer
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+	result = a * b;
+	return (result);
+}
