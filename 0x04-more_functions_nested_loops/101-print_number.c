d02e4aa17fa7e6cbf071faf4af5dfb68d6ee347a
Mohammed <mohammedthe11th@gmail.com>
2022-10-21 22:01:46 +0300

    some fixes

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,37 +1,43 @@
 #include "main.h"
 
 /**
- * print_number - prints given 4 digits integers using _putchar
+ * print_number - prints given integers using _putchar
  * @n: is an integer to be printed
  * Return: rerurs void
  */
 
 void print_number(int n)
 {
+	int m;
+
 	if (n < 0)
 	{
-		n = -n;
+		m  = n * -1;
 		_putchar('-');
 	}
-	if (n > 999)
+	else
 	{
-		_putchar((n / 1000) + '0');
-		_putchar((n % 1000) / 100 + '0');
-		_putchar(((n % 1000) % 100) / 10 + '0');
-		_putchar(((n % 1000) % 100) % 10 + '0');
+		m = n;
 	}
-	else if (n > 99)
+	if (m > 999)
 	{
-		_putchar((n / 100) + '0');
-		_putchar((n % 100) / 10 + '0');
-		_putchar((n % 100) % 10 + '0');
+		_putchar((m / 1000) + '0');
+		_putchar((m % 1000) / 100 + '0');
+		_putchar(((m % 1000) % 100) / 10 + '0');
+		_putchar(((m % 1000) % 100) % 10 + '0');
 	}
-	else if (n > 9)
+	else if (m > 99)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		_putchar((m / 100) + '0');
+		_putchar((m % 100) / 10 + '0');
+		_putchar((m % 100) % 10 + '0');
 	}
-	else if (n >= 0 && n <= 9)
+	else if (m > 9)
+	{
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
+	}
+	else
 	{
 		_putchar(n + '0');
 	}
