057fb3a8d04104fcae8abf3f7d2d1df3bb12dc90
Mohammed <mohammedthe11th@gmail.com>
2022-10-21 23:33:18 +0300

    with recursion

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,7 +3,7 @@
 /**
  * print_number - prints given integers using _putchar
  * @n: is an integer to be printed
- * Return: rerurs void
+ * Return: returns void
  */
 
 void print_number(int n)
@@ -12,33 +12,17 @@ void print_number(int n)
 
 	if (n < 0)
 	{
+		m = -n;
 		_putchar('-');
-		m  = n * -1;
 	}
-	else
+	if (n == 0)
 	{
-		m = n;
+		_putchar('0');
 	}
-	if (m > 999)
+	m = n;
+	if ((m / 10) > 0)
 	{
-		_putchar((m / 1000) + 48);
-		_putchar((m % 1000) / 100 + 48);
-		_putchar(((m % 1000) % 100) / 10 + 48);
-		_putchar(((m % 1000) % 100) % 10 + 48);
+		print_number(m / 10);
 	}
-	else if (m > 99)
-	{
-		_putchar((m / 100) + 48);
-		_putchar((m % 100) / 10 + 48);
-		_putchar((m % 100) % 10 + 48);
-	}
-	else if (m > 9)
-	{
-		_putchar((m / 10) + 48);
-		_putchar((m % 10) + 48);
-	}
-	else
-	{
-		_putchar(m + 48);
-	}
+	_putchar(n % 10 + '0');
 }
