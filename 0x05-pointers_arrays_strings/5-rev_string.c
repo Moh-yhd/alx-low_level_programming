3e1610ea6c5733ae130c719f6f22775a52835660
Mohammed <mohammedthe11th@gmail.com>
2022-10-25 15:42:28 +0300

    swapping should be handled carefully

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,30 +1,30 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * rev_string - prints a string in reverse
  * @s: is a string
  * Return: returns nothing
  */
 
-int main()
+void rev_string(char *s)
 {
-	char s[] = "My school";
-	int i = 0;
-	int j;
-	int k;
-	
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	k = i - 1;
-	char rev[i];
+	char rev[1000], exc[1000];
+	int j = 0, len = 0, i, k;
 
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	k = len;
+	len = len - 1;
 	for (j = 0; j < k; j++)
 	{
-		rev[j] = s[i];
-		i--;
+		rev[j] = s[len];
+		len--;
 	}
-	rev[j] = '\0';
-	printf("%s\n", rev);
+	for (i = 0; i < k; i++)
+	{
+		exc[i] = rev[i] + s[i];
+		s[i] = exc[i] - s[i];
+	}
 }
