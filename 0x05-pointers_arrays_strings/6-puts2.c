4e14175ac623ab3f63dccfd7105377e2aca4f1ea
Mohammed_Android <mohammedthe11th@gmail.com>
2022-10-25 06:48:39 +0300

    with if and else cases

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,14 +6,44 @@
  * Return: nothing
  */
 
+int _strlen(char *s);
+
 void puts2(char *str)
 {
 	int i = 0;
+	int count;
 
-	while (str[i] != '\0')
+	count = _strlen(str);
+	if (count % 2 == 0)
 	{
-		_putchar(str[i]);
-		++i;
+		while (str[i] != '\0')
+		{
+			_putchar(str[i]);
+			i = i + 2;
+		}
+	}
+
+	else if (count % 2 != 0)
+	{
+		while (str[i] != '\0')
+		{
+			if (i != count - 1)
+			{
+				_putchar(str[i]);
+				i = i + 1;
+			}
+		}
 	}
 	_putchar('\n');
+}
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
