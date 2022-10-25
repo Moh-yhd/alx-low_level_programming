d365211f60969a2b991a0de18650ef6fef1b418b
Mohammed <mohammedthe11th@gmail.com>
2022-10-25 16:21:42 +0300

    including integer printing function

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * print_array - prints the first n elements of an array
+ * @a: is an array
+ * Return: nothing
+ */
+
+void print_number(int n);
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		print_number(a[i]);
+		if (i < (n - 1))
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
+
+void print_number(int n)
+{
+	int m;
+	int reverse = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = n * -1;
+	}
+	else if (n > 0)
+	{
+		m = n;
+	}
+	while (m > 0)
+	{
+		reverse = reverse * 10 + m % 10;
+		m = m / 10;
+	}
+	while (reverse > 0)
+	{
+		_putchar(reverse % 10 + '0');
+		reverse = reverse / 10;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+}
