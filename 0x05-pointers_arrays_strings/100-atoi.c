84fdc90ae634fc8f9067d7eb3263bd05b7db57b2
Mohammed <mohammedthe11th@gmail.com>
2022-10-25 22:49:57 +0300

    chnaging char in to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - changes a string in to an integer
+ * @s: s is a string
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int i = 0, j = 0, k = 0, l = 0, m;
+	int number = 0;
+
+	while (s[i] != '\n')
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			k = s[i] - '0';
+			number = number * 10 + k;
+			j++;
+		}
+		if (j > 0 && (s[i] < 48 || s[i] > 57))
+			break;
+		i++;
+	}
+	for (m = 0; m < (i - j); m++)
+	{
+		if (s[m] == 45)
+			l++;
+	}
+	if (l % 2 != 0)
+		number = number * -1;
+	return (number);
+}
