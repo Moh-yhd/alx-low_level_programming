b1f8277f970f04b94da8254c8a02ceae3fcb82f7
Mohammed <mohammedthe11th@gmail.com>
2022-10-25 18:38:48 +0300

    betty issues fixed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,11 +2,12 @@
 
 /**
  * print_array - prints the first n elements of an array
- * @a: is an array
  * Return: nothing
+ * print_number -  prints an integer using _putchar
+ * @m: is an integer
  */
 
-void print_number(int n);
+void print_number(int m);
 void print_array(int *a, int n)
 {
 	int i;
@@ -23,21 +24,25 @@ void print_array(int *a, int n)
 	_putchar('\n');
 }
 
-void print_number(int n)
+/**
+ * print_number -  prints an integer using _putchar
+ * @m: is an integer
+ */
+
+void print_number(int m)
 {
-	if (n < 0)
+	if (m < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		m = m * -1;
 	}
-	
-	if (n == 0)
+	if (m == 0)
 	{
 		_putchar('0');
 	}
-	if (n / 10)
+	if (m / 10)
 	{
-		print_number(n / 10);
+		print_number(m / 10);
 	}
-	_putchar(n % 10 + '0');	
+	_putchar(m % 10 + '0');
 }
