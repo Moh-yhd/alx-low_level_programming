2619205c2a823fafb438465fba692e6099c40b82
Mohammmed <mohammedthe11th@gmail.com>
2022-10-28 22:49:35 +0300

    checker and codder

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string in to 1337
+ * @s: is a string
+ * Return: a 1337 encoded string
+ */
+
+char *leet(char *s)
+{
+	checker = "aeotl";
+	codder = "43071";
+	int i, j;
+
+	for (i = 0; checker[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] '\0'; j++)
+		{
+			if (s[j] == checker[i] || s[j] == checker[i] - 32)
+			{
+				s[j] = codder[i];
+			}
+		}
+	}
+}
