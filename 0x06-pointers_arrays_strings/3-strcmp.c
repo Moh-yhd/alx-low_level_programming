32c0a59311c89171bca8b20fa19e9f2621e695f6
Mohammmed <mohammedthe11th@gmail.com>
2022-10-28 23:10:44 +0300

    issues fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0;
+	int i;
 	int flag;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
@@ -25,7 +25,7 @@ int _strcmp(char *s1, char *s2)
 		flag = -1;
 	if (s1[i] > s2[i])
 		flag = 1;
-	if (s1[j] == s2[j])
+	if (s1[i] == s2[i])
 		flag = 0;
 	return (flag);
 }
