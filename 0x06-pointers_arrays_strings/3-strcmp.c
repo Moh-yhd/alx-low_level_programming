30cd6673bd3f7b583897762f34ce16226023a51e
Mohammmed <mohammedthe11th@gmail.com>
2022-10-28 23:03:20 +0300

    short code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,13 +12,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0, j = 0;
-	int count1 = 0, count2 = 0;
 	int flag;
-
-	while (s1[count1] != '\0')
-	count1++;
-	while (s2[count2] != '\0')
-	count2++;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
@@ -31,13 +25,7 @@ int _strcmp(char *s1, char *s2)
 		flag = -1;
 	if (s1[i] > s2[i])
 		flag = 1;
-	if (count1 == count2)
-	{
-		for (j = 0; j < i; j++)
-		{
-			if (s1[j] == s2[j])
-				flag = 0;
-		}
-	}
+	if (s1[j] == s2[j])
+		flag = 0;
 	return (flag);
 }
