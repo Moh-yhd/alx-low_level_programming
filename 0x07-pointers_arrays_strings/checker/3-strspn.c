614c5fc9fc38df01f17284c0adbb7fb697d8474d
Mohammmed <mohammedthe11th@gmail.com>
2022-10-31 22:36:31 +0300

    strspn

---
+++ 0x07-pointers_arrays_strings/checker/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strspn - returns the number of bytes in the innitial segement of s
+ * that contains all bytes from accept.
+ * @s: is a string consisting the initial segement. 
+ * @accept: is the string that contains the bytes to be checked
+ * Return: the number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (accept [j] == s[i])
+				break;
+		}
+	}
+	return (i + 1);
+}
