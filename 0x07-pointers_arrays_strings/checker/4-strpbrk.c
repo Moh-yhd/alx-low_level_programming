feae2fe4fd106351360c90ab351f66a767d264bf
Mohammmed <mohammedthe11th@gmail.com>
2022-11-01 00:07:00 +0300

    using flag to breakout of a nested loop

---
+++ 0x07-pointers_arrays_strings/checker/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - returns the first occurence of any byte of sting accept on
+ * string s.
+ * @s: is a string to look for the first occurence of byte of sting  accept
+ * @accept: is a string whose occurence of any of its bytes in s is to be
+ * returned.
+ * Return: returns a pointer to the byte in s that matches accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+	int flag;
+	char *p;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				flag = 1;
+		}
+		if (flag == 1)
+			break;
+	}
+	p = s + i;
+	return (p);
+}
+
