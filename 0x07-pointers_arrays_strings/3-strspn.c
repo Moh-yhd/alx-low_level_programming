5a7b8a7a5719d9f032127f2a005f9f5b471fd5a5
Mohammmed <mohammedthe11th@gmail.com>
2022-10-31 22:39:46 +0300

    srtspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,7 +3,7 @@
 /**
  * _strspn - returns the number of bytes in the innitial segement of s
  * that contains all bytes from accept.
- * @s: is a string consisting the initial segement. 
+ * @s: is a string consisting the initial segement.
  * @accept: is the string that contains the bytes to be checked
  * Return: the number of bytes
  */
@@ -16,7 +16,7 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; s[j] != '\0'; j++)
 		{
-			if (accept [j] == s[i])
+			if (accept[j] == s[i])
 				break;
 		}
 	}
