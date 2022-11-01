d694ec3baf5e2fabe61c75794d6a5c9ae4916ba1
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-01 07:31:54 +0300

    strspn

--- 0x07-pointers_arrays_strings/checker/3-strspn.c
+++ 0x07-pointers_arrays_strings/checker/3-strspn.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strspn - returns the number of bytes in the innitial segement of s
  * that contains all bytes from accept.
@@ -10,15 +10,23 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
-
+	unsigned int i, j, len = 0, flag = 0;
+	while (accept[len] != '\0')
+		len++;
 	for (i = 0; accept[i] != '\0'; i++)
 	{
 		for (j = 0; s[j] != '\0'; j++)
 		{
-			if (accept [j] == s[i])
-				break;
+			if (accept [i] == s[j])
+			{
+				flag++;
+			}
+			printf("s[%u] = %c accept[%u] = %c\n", j, s[j], i, accept[i]);
+			printf("j= %u - %u = i\nflag = %u\n", j, i, flag);
 		}
+		if (flag == len)
+			break;
+		flag = 0;
 	}
-	return (i + 1);
+	return (i);
 }
