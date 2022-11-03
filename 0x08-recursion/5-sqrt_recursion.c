f02c3815547fd56580d28400b1f7360c1459c7c6
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-03 17:05:05 +0300

    betty fixed

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,6 +2,7 @@
 
 /**
  * _sqrt_recursion - claculates the square root of an integer.
+ * _sqrt - returns the square root of a number
  * @n: is an integer
  * Return: the square of n, if it exists, and -1 if it doesn't exist
  */
@@ -11,6 +12,12 @@ int _sqrt_recursion(int n)
 {
 	return (_sqrt(n, 1));
 }
+
+/**
+ * _sqrt - returns the square root of a function
+ * @m: is an integer
+ * Return: int
+ * */
 
 int _sqrt(int n, int  m)
 {
