68c2e9f15f0f07ebc8da58dd17d3f3e420880319
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-03 10:38:29 +0300

    power function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calcules the result of x the power of y
+ * @x: is an integer
+ * @y: is an integer
+ * Return: an integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return x * _pow_recursion(x, y - 1);
+	}
+}
