83f8b830650a86eda70d18ab438930362ae12193
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-04 20:24:13 +0300

    using atoi function to convert char to int

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the programs's name
+ * @argc: is the number of arguments to the main
+ * @argv: an array containg pointers to the argument strings
+ * Return: 0 if success
+ */
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	int mul, arg1, arg2;
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[2]);
+	mul = arg1 * arg2;
+	printf("%d %d\n%d\n", arg1, arg2, mul);
+	return (0);
+}
