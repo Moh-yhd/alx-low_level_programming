eff11d8d9c6f77268a51371f61d15cf5ebb5bb07
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-04 16:07:15 +0300

    number of arguments without the file name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints the programs's name
+ * @argc: is the number of arguments to the main
+ * @argv: an array containg pointers to the argument strings
+ * Return: 0 if success
+ */
+
+int main(int __attribute__ ((unused)) argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
