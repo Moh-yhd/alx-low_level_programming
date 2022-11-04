40efbb962c825861117bef1f3a83bb079f573f4c
Mohammmed <mohammedthe11th@gmail.com>
2022-11-04 22:12:52 +0300

    Error case added

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,9 +13,16 @@ int main(int __attribute__ ((unused)) argc, char *argv[])
 {
 	int mul, arg1, arg2;
 
+	if (argc < 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[2]);
 	mul = arg1 * arg2;
 	printf("%d\n", mul);
+	}
 	return (0);
 }
