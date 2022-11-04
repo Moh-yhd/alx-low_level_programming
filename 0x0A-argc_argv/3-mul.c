403bb7c881a90d1c153e6fdf99f37e0a8aee1682
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-04 20:29:16 +0300

    using atoi function to convert char to int

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,6 +16,6 @@ int main(int __attribute__ ((unused)) argc, char *argv[])
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[2]);
 	mul = arg1 * arg2;
-	printf("%d %d\n%d\n", mul);
+	printf("%d\n", mul);
 	return (0);
 }
