17dd9a1c61bbee387ae4a90334ae8a32560d329c
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-04 16:11:52 +0300

    number of arguments without the file name

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
  * Return: 0 if success
  */
 
-int main(int __attribute__ ((unused)) argc, char __attribute__ ((unused)) *argv[])
+int main(int argc, char __attribute__ ((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
