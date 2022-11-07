4c0079d2d869882789d675beab0872a750d5d450
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-07 20:27:04 +0300

    copying a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdilb.h>
+#include <stdio.h>
+
+/**
+ * _strdup - copies a sting to a memory location and retunlrns the pointer for the memory allocated
+ * @str: is a string
+ *
+ * Return: pointer to a memory location
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char ar;
+
+	if (*str == NULL)
+	{
+		return (NULL);
+	}
+	ar = malloc(sizeof(str));
+	for (i = 0; *str != '\0'; i++)
+	{
+		ar[i] = str[i];
+	}
+	return (ar);
+	free(ar);
+}
