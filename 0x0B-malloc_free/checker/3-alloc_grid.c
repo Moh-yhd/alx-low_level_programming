356d594ee26915ee7e1ab99e83edfe190c7813ae
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-08 08:47:23 +0300

    alloxating memory for a 2d array

---
+++ 0x0B-malloc_free/checker/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - allocates a memory for a two dimenaional allar and fills it with zero.
+ * @width: is the width of the memory grid
+ * @height: is the height if the memory gris
+ *
+ * Return: integer pointer to the allocated memory grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ar;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	ar = malloc(height * sizeof(int *));
+	if (ar == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		ar[i] = malloc(width * sizeof(int));
+		if (ar[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ar[i][j] = 0;
+		}
+	}
+	return (ar);
+}
--- 0x0B-malloc_free/checker/main.h
+++ 0x0B-malloc_free/checker/main.h
@@ -2,3 +2,4 @@ char *create_array(unsigned int, char);
 char *_strdup(char *);
 int _len(char *);
 char *str_concat(char *, char *);
+int **alloc_grid(int, int);
