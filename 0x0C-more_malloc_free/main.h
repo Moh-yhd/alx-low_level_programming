9a90b631eb5241afd3cb5818cb8394bfeeb053cf
Mohammmed <mohammedthe11th@gmail.com>
2022-11-10 22:18:02 +0300

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,9 @@
-void *malloc_checked(unsigned int b);
+#ifndef MAIN_H
+#define MAIN_H
+
+void *malloc_checked(unsigned int);
 char *string_nconcat(char *, char *, unsigned int);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 
+#endif
