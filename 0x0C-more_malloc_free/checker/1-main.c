163061a917c452cdbb9d27d8bcd6150e098338f9
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-10 13:50:36 +0300

    null case handled

--- 0x0C-more_malloc_free/checker/1-main.c
+++ 0x0C-more_malloc_free/checker/1-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - check the code
@@ -11,8 +12,9 @@ int main(void)
 {
     char *concat;
 
-    concat = string_nconcat("Best ", "School !!!", 6);
+    concat = string_nconcat(NULL, NULL, 50);
     printf("%s\n", concat);
     free(concat);
+    printf("%lu\n",strlen(concat));
     return (0);
 }
--- 0x0C-more_malloc_free/checker/1-string_nconcat.c
+++ 0x0C-more_malloc_free/checker/1-string_nconcat.c
@@ -16,6 +16,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = 0;
 	char *p;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
@@ -27,7 +31,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		free(p);
 	}
 	for (i = 0; i < len1; i++)
+	{
 		p[i] = s1[i];
+	}
 	for (j = 0; i < (len1 + n) && s2[j] != '\0'; j++, i++)
 		p[i] = s2[j];
 	p[i] = '\0';
