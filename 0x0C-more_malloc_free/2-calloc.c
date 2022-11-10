d60634bd42d3552ae6291a22bf371232af0d59c1
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-10 15:10:28 +0300

    proper pointer castng

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	void *p;
+	int *p;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 	for (i = 0; i < nmemb * size; i++)
-		*((char *)(p) + i) = 0;
-	return (p);
+		p[i] = 0;
+	return ((void *)p);
 }
 
