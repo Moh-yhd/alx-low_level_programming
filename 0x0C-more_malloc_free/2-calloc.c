a24b704730de1984812ff7d63557015ba5ac7a94
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-10 14:58:48 +0300

    malloc only

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,12 +15,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 
 	if (nmemb == 0 || size == 0)
-		return ('\0');
+		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 	{
-		return ('\0');
-		free(p);
+		return (NULL);
 	}
 	for (i = 0; i < nmemb * size; i++)
 		p[i] = '0';
