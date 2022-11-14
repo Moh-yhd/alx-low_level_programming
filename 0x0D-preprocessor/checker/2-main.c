2757152da55bbe731a22db2232b5ee315b9e9f7d
Mohammmed <mohammedthe11th@gmail.com>
2022-11-14 22:20:25 +0300

    macro __FILE__

---
+++ 0x0D-preprocessor/checker/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the file name it was compiled on
+ * 
+ * Return: always zero
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
