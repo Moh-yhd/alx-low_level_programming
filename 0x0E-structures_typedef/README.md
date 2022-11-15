3e323820166609f586fd6446130e1aebc08ac4d8
Mohammmed <mohammedthe11th@gmail.com>
2022-11-15 20:10:55 +0300

    first struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains practice tasks about structures and type definationa in C.
---
+++ 0x0E-structures_typedef/god.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - creates a structure called dog
+ * @name: is a pointer to a string conataining the name of the dog
+ * @age: is an integer corrosponding to the age of the dog
+ * @owner: is a pointerto a string containing the name of the owner of the dog
+ *
+ * Description: this structure creates a data structure for saving a dog's
+ * name, its age, and its owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
