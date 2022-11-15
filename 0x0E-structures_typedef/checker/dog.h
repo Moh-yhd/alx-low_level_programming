13f1ffd55bbdc83e5b84b1d761115797e738691a
Mohammmed <mohammedthe11th@gmail.com>
2022-11-15 20:24:28 +0300

    proper syntax for struct

---
+++ 0x0E-structures_typedef/checker/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - creates a structure called dog
+ * @name: is a pointer to a string conataining the name of the dog
+ * @age: is an integer corrosponding to the age of the dog
+ * @owner: is a pointerto a string containing the name of the owner of the dog
+ *
+ * Description: this structure creates a data structure for saving a dog's
+ * name, its age, and its owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+#endif
