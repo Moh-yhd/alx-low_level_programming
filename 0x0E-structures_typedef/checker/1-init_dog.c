e48d81b3834f5781a54aef7020183731af2faadf
Mohammmed <mohammedthe11th@gmail.com>
2022-11-15 20:38:55 +0300

    initializing

---
+++ 0x0E-structures_typedef/checker/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialized a variable of type struct dog
+ * @d: is a a pointer to the struct dog
+ * @name: is a pointer to a string containig the name of the dog
+ * @age: is a float corrosponding to the age of the dog
+ * @owner: is a pointer to a string containing the owner of the dog
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	*d.name = name;
+	*d.age = age;
+	*d.owner = owner;
+}
