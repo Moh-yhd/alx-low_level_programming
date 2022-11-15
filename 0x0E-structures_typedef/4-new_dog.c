47ac16693d6d9e15e43d6fb18d4b599b0244a570
Mohammmed <mohammedthe11th@gmail.com>
2022-11-15 22:21:35 +0300

    strings are null terminated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -39,8 +39,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(my_dog);
 		free(my_dog->name);
 	}
-	my_dog->name = name;
+	copy_str(name, my_dog->name);
 	my_dog->age = age;
-	my_dog->owner = owner;
+	copy_str(owner, my_dog->owner);
 	return (my_dog);
+}
+char *copy_str(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s2[i] = s1[i];
+	}
+	s2[i] = '\0';
+
+	return (s2);
 }
