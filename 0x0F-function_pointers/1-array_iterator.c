d2420746bf1136e306d3e91464a28b3aa25ad6f4
Mohammmed <mohammedthe11th@gmail.com>
2022-11-16 12:26:24 +0300

    betty fixed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@
  * array_iterator - applies a function on each elements of an array
  * @array: is the array containing elements a function is to act on
  * @size: is a pointer to a struct of type size_t
- * @action: is a pointer ot a funtion 
+ * @action: is a pointer ot a funtion
  *
  * Return: nothing
  */
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 
-	if ( array != NULL && action != NULL)
+	if (array != NULL && action != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
