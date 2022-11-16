44a8733cae15277be3577e6faf5a34c4950d35f6
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-16 20:59:40 +0300

    checker

---
+++ 0x0F-function_pointers/checker/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - is a function that searches for an int
+ * @array: is a pointer to an array 
+ * @size: is the number of elements in array arrray
+ * cmp: a pointer to a function that compares
+ *
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if(cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
