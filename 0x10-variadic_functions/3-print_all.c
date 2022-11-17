22550c5eedb3e62bb13a67e63915092a5af7c63d
Mohammmed <mohammedthe11th@gmail.com>
2022-11-18 00:46:50 +0300

    betty

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,7 +3,12 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
-
+/**
+ * print_all - prints arguments passed to it
+ * @format: is the type of the arguments passed
+ *
+ * Return: nothing
+ */
 void print_all(const char * const format, ...)
 {
 	int i, j;
@@ -15,7 +20,6 @@ void print_all(const char * const format, ...)
 		{'s', print_str},
 		{'\0', NULL}
 	};
-	
 	va_start(ap, format);
 	i = 0;
 	while (format && format[i] != '\0')
@@ -25,7 +29,7 @@ void print_all(const char * const format, ...)
 		{
 			if (format[i] == fun_list[j].format)
 			{
-				if(i)
+				if (i)
 				printf(", ");
 				fun_list[j].f(&ap);
 			}
@@ -37,18 +41,45 @@ void print_all(const char * const format, ...)
 	printf("\n");
 }
 
+/**
+ * print_int - prints integers
+ * @ap: ia an argument
+ *
+ * Return: nothing
+ */
 void print_int(va_list *ap)
 {
-	printf("%d", va_arg(*ap, int ));
+	printf("%d", va_arg(*ap, int));
 }
+
+/**
+ * print_str - prints strings
+ * @ap: ia an argument
+ *
+ * Return: nothing
+ */
 void print_str(va_list *ap)
 {
 	printf("%s", va_arg(*ap, char *));
 }
+
+/**
+ * print_char - prints chars
+ * @ap: ia an argument
+ *
+ * Return: nothing
+ */
 void print_char(va_list *ap)
 {
 	printf("%c", va_arg(*ap, int));
 }
+
+/**
+ * print_float - prints floats
+ * @ap: ia an argument
+ *
+ * Return: nothing
+ */
 void print_float(va_list *ap)
 {
 	printf("%f", va_arg(*ap, double));
