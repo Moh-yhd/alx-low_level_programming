e5a21e854f5130f877ca5b16fb313665777bd29f
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-17 12:10:26 +0300

    first variadic function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,7 +5,7 @@
  * sum_them_all - sums all arguments passed to it
  * @n: is an int argument
  *
- * Return: the sum of argumenta
+ * Return: the sum of arguments
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -13,11 +13,9 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	va_list ap;
 
-
 	if (n == 0)
 		return (0);
 	va_start(ap, n);
-	
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ap, int);
