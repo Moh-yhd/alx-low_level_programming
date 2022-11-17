4c546fb4f87fe962955356700c91463a49a8f4e9
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-17 16:56:19 +0300

    count

---
+++ 0x10-variadic_functions/checker/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: is a pointer to the separator char
+ * @n: is a one of the numbers to be print
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d",va_arg(ap, unsigned int));
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
