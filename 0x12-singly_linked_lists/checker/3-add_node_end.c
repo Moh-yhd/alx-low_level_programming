180f8d0b2926f5cb7516d925102aa4335f8ab1ac
Mohammmed <mohammedthe11th@gmail.com>
2022-11-24 23:53:24 +0300

    seg fault

---
+++ 0x12-singly_linked_lists/checker/3-add_node_end.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds node at the end of a list
+ * @head: is a pointer to the pointer of the start of a list
+ * @str: is atring
+ *
+ * Return: pointer to the head of the list
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *ptr;
+	list_t *temp;
+	unsigned int length = 0;
+
+
+	if (str == NULL)
+		return (NULL);
+	while (str[length])
+		length++;
+
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr->str = strdup(str);
+	ptr->len = length;
+	ptr->next = NULL;
+
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = ptr;
+
+	return (ptr);
+}
+	
+
