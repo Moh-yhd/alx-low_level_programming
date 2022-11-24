00583521a19cddc18b9e56ab665b696c0bece8c0
Mohammmed <mohammedthe11th@gmail.com>
2022-11-24 21:35:10 +0300

    header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,21 @@
 #ifndef _LISTS_H
 #define _LISTS_H
+
+/**
+ * struct list_s - creates a structure called list_t
+ * @str: a string
+ * @len: is len of the string
+ * @next: is a pointer
+ *
+ * Description: this structure have a string, its length
+ * and the next pointer to it
+ */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 
 size_t print_list(const list_t *);
 
