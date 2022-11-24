30611f23d40c904fc64de9d6fe5f43d66e214888
Mohammmed <mohammedthe11th@gmail.com>
2022-11-24 22:35:03 +0300

    null case added

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,6 +17,8 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[length])
 		length++;
 	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+		return (NULL);
 
 	ptr->str = strdup(str);
 	ptr->len = length;
