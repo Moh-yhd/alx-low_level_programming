d38b63ed6e80412a5825abc27ec8e9aa439b21f1
Mohammmed <mohammedthe11th@gmail.com>
2022-11-29 08:15:28 +0300

    empty case handled

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,9 +11,15 @@ int pop_listint(listint_t **head)
 	listint_t *ptr;
 	int n;
 
-	ptr = *head;
-	n = ptr->n;
-	*head = (ptr->next)->next;
-	free(ptr);
+	if (head == NULL || *head == NULL)
+		return (0);
+	else 
+	{
+		ptr = *head;
+		n = ptr->n;
+		*head = (ptr->next)->next;
+		free(ptr);
+	}
+
 	return (n);
 }
