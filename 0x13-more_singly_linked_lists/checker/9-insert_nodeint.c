71ddabde5b8b91f8455423118b50a3d3067ce48c
Mohammed_Android <mohammedthe11th@gmail.com>
2022-11-29 20:35:23 +0300

    9

---
+++ 0x13-more_singly_linked_lists/checker/9-insert_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts a
+ * node at an index
+ * @head: points to the head
+ * @idx: is an index where a node is to 
+ * to be inserted
+ * @n: is the data of the node
+ *
+ * Return: pointer to the added node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *ptr, temp;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+		return (NULL);
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr->n = n;
+	temp= *head;
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+		if (count == idx)
+		{
+			ptr->next = temp; 
+			temp->next = ptr;
+
+
+
