5a6307def215f2fa335682ba2981e5a09a49caf7
Mohammmed <mohammedthe11th@gmail.com>
2022-11-29 22:06:19 +0300

    null case added

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr, *temp;
 	unsigned int count = 0;
 
-	if (*head == NULL)
+	if (*head == NULL && idx != 0)
 		return (NULL);
 	ptr = malloc(sizeof(listint_t));
 	if (ptr == NULL)
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			ptr->next = temp->next;
 			temp->next = ptr;
-			return (*head);
+			return (*ptr);
 		}
 	}
 
