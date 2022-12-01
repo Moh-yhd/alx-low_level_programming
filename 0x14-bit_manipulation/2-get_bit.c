b46dcac1ba5adae0df142d0fdd5956b1384fd667
Mohammmed <mohammedthe11th@gmail.com>
2022-12-01 23:43:34 +0300

    getting index value

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit at any given index
+ * @n: is a long integer
+ * @index: is where the bit value is required
+ *
+ * Return: index value (int)
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+	int b;
+
+	for (i = 63; ; i--)
+	{
+		b = (n >> i) & 1;
+		if (i == index)
+			return (b);
+	}
+	return (-1);
+}
