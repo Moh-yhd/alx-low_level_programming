41b2701d4243b48ff1d1b05105b8421f30718a93
Mohammed_Android <mohammedthe11th@gmail.com>
2022-12-01 14:55:32 +0300

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_unint - converts binary to
+ * unsigned int
+ * @b: is a pointer to a string of a
+ * string of binary number
+ * Return: converted unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n;
+	int i, twos = 1;
+	int len = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[len] != '\0')
+		len++;
+
+	n = 0;
+	for (i = (len - 1); i >= 0; i--)
+	{
+		if (b[i] != 48 && b[i] != 49)
+			return (0);
+		n = n + (b[i] - '0') * twos;
+		twos = twos * 2;
+	}
+	return (n);
+}
+
+
