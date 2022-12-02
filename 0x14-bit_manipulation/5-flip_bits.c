022c47a06bc1674445f983dd0cf7eaa8b83b4dfd
Moh-yhd <113825141+Moh-yhd@users.noreply.github.com>
2022-12-02 08:51:42 +0300

    Update 5-flip_bits.c
    index limit changed

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,9 +10,12 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int i, count = 0;
+	unsigned long int limit;
 	unsigned long int a, b;
+	
+	limit = sizeof(unsigned long int) * 8;
 
-	for (i = 0; i < 63; i++)
+	for (i = 0; i < limit; i++)
 	{
 		a = (n >> i) & 1;
 		b = (m >> i) & 1;
