cdcba590bfa16408f2ee7b07de498ce6bce113ef
Mohammmed <mohammedthe11th@gmail.com>
2022-12-02 10:36:51 +0300

    index limit checked

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	int b;
 
 	limit = sizeof(unsigned long int) * 8;
-	if (i > limit)
+	if (index > limit)
 		return (-1);
 
 	for (i = limit; ; i--)
