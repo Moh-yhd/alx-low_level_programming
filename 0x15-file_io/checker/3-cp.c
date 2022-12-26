87b4b4d2f2cbc2a1b9b94f32924ff51d128156da
Mohammmed <mohammedthe11th@gmail.com>
2022-12-26 15:49:09 +0300

    copy files

--- 0x15-file_io/checker/3-cp.c
+++ 0x15-file_io/checker/3-cp.c
@@ -5,54 +5,74 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+void err_pr(int a, int b);
+
 /**
  * main - copies a file's content to another file
  * @ac: is the number of arguments passed to the function
  * @av: is the a pointer to a pointer of strings passes as arguments
  *
- * Return: 1 if success and -1 if failure
+ * Return: 0 if success and -1 if failure
  */
 int main(int ac, char **av)
 {
 	int fd1, fd2, a, b;
 	ssize_t r_bytes, w_bytes;
-	char *err1 = "Usage: cp file_from file_to\n";
-	char *err2 = "Error: Can't read from file NAME_OF_THE_FILE\n";
-	char *err3 =
-	"where NAME_OF_THE_FILE is the first argument passed to your program\n";
-	char *err4 = "Error: Can't write to NAME_OF_THE_FILE\n";
-	char *err5 = "Error: Can't close fd FD_VALUE\n";
 	char buff[1024];
 
-	if (ac < 3)
+	if (ac != 3)
 	{
-		write(STDERR_FILENO, err1, 28);
+		dprintf(STDERR_FILENO, "Usage: cp file_from tile_to\n");
 		exit(97);
 	}
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		write(STDERR_FILENO, err2, 45);
-		write(STDERR_FILENO, err3, 68);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	r_bytes = read(fd1, buff, 1024);
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	w_bytes = write(fd2, buff, 1024);
-	if (fd2 == -1 || r_bytes != w_bytes)
+	if (fd2 == -1)
 	{
-		write(STDERR_FILENO, err4, 40);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
 		exit(99);
+	}
+	while ((r_bytes = read(fd1, buff, 1024)) > 0)
+	{
+		w_bytes = write(fd2, buff, r_bytes);
+		if (r_bytes != w_bytes)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
+			exit(99);
+		}
+		if (r_bytes == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
 	}
 	a = close(fd1);
 	b = close(fd2);
+	err_pr(a, b);
+	return (0);
+}
+
+/**
+ * err_pr - prints error message
+ * @a: is an integer
+ * @b: is an integer
+ * Return: nothing
+ */
+
+void err_pr(int a, int b)
+{
 	if (a == -1 || b == -1)
 	{
-		write(STDERR_FILENO, err5, 32);
+		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE");
 		exit(100);
 	}
-	return (1);
 }
+
 
 
 
