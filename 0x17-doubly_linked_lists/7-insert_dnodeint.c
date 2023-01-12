fc9385e23768fb11a0956935f9436e4932666f20
Mohammmed <mohammedthe11th@gmail.com>
2023-01-12 23:35:35 +0300

    insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	for (i = 0; i < idx - 1; i++)
-		head= head->next;
+		head = head->next;
 
 	if (head == NULL || head->next == NULL)
 		return (NULL);
