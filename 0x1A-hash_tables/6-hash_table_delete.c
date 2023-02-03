50a6b3e239e992269b7dbf065935934d58a07f01
Mohammmed <mohammedthe11th@gmail.com>
2023-02-04 00:36:51 +0300

    free at last

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,27 +11,29 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *tmp, *tmp2;
 	unsigned long int i;
 
-	if (ht != NULL)
+	if (ht == NULL)
+		return;
+
+
+	for (i = 0; i < ht->size; i++)
 	{
-		for (i = 0; i < ht->size; i++)
+		if (ht->array[i])
 		{
-			if (ht->array[i] != NULL)
-			{
-			tmp = ht->array[i]->next;
-			while (tmp)
-			{
-				tmp2 = tmp;
-				tmp = tmp->next;
-				free(tmp2->key);
-				free(tmp2->value);
-				free(tmp2);
-			}
-			free(ht->array[i]->key);
-			free(ht->array[i]->value);
-			free(ht->array[i]);
-			}
+		tmp = ht->array[i]->next;
+		while (tmp)
+		{
+			tmp2 = tmp;
+			tmp = tmp->next;
+			free(tmp2->key);
+			free(tmp2->value);
+			free(tmp2);
 		}
-		free(ht->array);
-		free(ht);
+		free(ht->array[i]->key);
+		free(ht->array[i]->value);
+		free(ht->array[i]);
+		}
+
 	}
+	free(ht->array);
+	free(ht);
 }
