12dfba668d7b2872fac0b141c91e9afdbb22dc80
Mohammmed <mohammedthe11th@gmail.com>
2023-02-03 15:24:10 +0300

    hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -19,7 +19,7 @@ typedef struct hasn_node_s
 	char *key;
 	char *value;
 	struct hash_node_s *next;
-} hash_node_s;
+} hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -34,5 +34,7 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
 
 #endif
